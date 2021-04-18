6136374981575760c12087733400d298d11a4356
Arber Muca <if19b164@technikum-wien.at>
2021-04-18 13:35:08 +0200

    Tour Model created

--- src/sample/models/Tour.java
+++ src/sample/models/Tour.java
@@ -1,4 +1,22 @@
 package sample.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Tour {
+    @Getter @Setter private String t_Name;
+    @Getter @Setter private double t_Distance;
+    @Getter @Setter private String startPoint;
+    @Getter @Setter private String destination;
+    @Getter @Setter private String description;
+
+    Tour(){}
+    Tour(String name, String desc, String start, String dest, double distance){
+
+        t_Name = name;
+        description = desc;
+        startPoint = start;
+        destination = dest;
+        t_Distance = distance;
+    }
 }
