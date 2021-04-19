99fc88584da1707d12ccf072dfb9548096baa49a
Arber Muca <if19b164@technikum-wien.at>
2021-04-19 18:22:10 +0200

    small changes

--- src/sample/views/AddTourController.java
+++ src/sample/views/AddTourController.java
@@ -1,4 +1,29 @@
 package sample.views;
 
-public class AddTourController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.*;
+import sample.viewModels.AddTourViewModel;
+
+public class AddTourController implements Initializable {
+
+    AddTourViewModel addTourViewModel = new AddTourViewModel();
+    public TextField NameTextField;
+    public TextField StartingPointTextField;
+    public TextField DestinationTextField;
+    public TextField DistanceTextField;
+    public TextArea DescriptionTextField;
+    public Button addBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Controller init");
+        NameTextField.textProperty().bindBidirectional(addTourViewModel.getInputName());
+        StartingPointTextField.textProperty().bindBidirectional(addTourViewModel.getInputStart());
+        DestinationTextField.textProperty().bindBidirectional(addTourViewModel.getInputDestination());
+        DistanceTextField.textProperty().bindBidirectional(addTourViewModel.getInputDistance());
+        DescriptionTextField.textProperty().bindBidirectional(addTourViewModel.getInputDescription());
+    }
 }
