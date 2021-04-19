2a7626329cf9e0e9ed9fa805e93ea4421a8aedd5
Arber Muca <if19b164@technikum-wien.at>
2021-04-19 18:21:23 +0200

    main window with other windows connected

--- src/sample/views/MainWindowController.java
+++ src/sample/views/MainWindowController.java
@@ -1,6 +1,11 @@
 package sample.views;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import sample.MenuModel;
 import sample.ReportGeneration;
+import sample.viewModels.HomeWindowViewModel;
 import sample.viewModels.LogViewModel;
 import sample.viewModels.TourViewModel;
 import javafx.event.ActionEvent;
@@ -10,21 +15,18 @@ import javafx.beans.binding.Bindings;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainWindowController implements Initializable{
-    public TourViewModel viewModel = new TourViewModel();
-    public LogViewModel logViewModel = new LogViewModel();
-    public MenuModel menuModel = new MenuModel();
-    public ReportGeneration reportGeneration = new ReportGeneration();
-    public int count = 0;
+    public HomeWindowViewModel viewModel = new HomeWindowViewModel();
     public TextField InputTextField;
     public Label OutputLabel;
 
     public MainWindowController()
     {
-        System.out.println("Controller created");
+        System.out.println("Main Controller");
     }
 
     @FXML
@@ -44,4 +46,86 @@ public class MainWindowController implements Initializable{
         InputTextField.textProperty().bindBidirectional(viewModel.inputProperty());
         Bindings.bindBidirectional(OutputLabel.textProperty(), viewModel.outputProperty());
     }
+
+    /**
+     * the method which connects the main window with
+     * the addTour window, when you click the add button
+     * */
+    public void addTourWindow(ActionEvent actionEvent) {
+        Parent root=null;
+        FXMLLoader loader = new FXMLLoader();
+
+        loader.setLocation(MainWindowController.class.getResource("addTour.fxml"));
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        AddTourController controller = loader.getController();
+        Stage stage = new Stage();
+        stage.setTitle("Add Tour");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+    /**
+     * The method is used to connect the main window with
+     * the editTour window, when you click the edit button
+     * */
+    public void editTourWindow(ActionEvent actionEvent) {
+        Parent root=null;
+        FXMLLoader loader = new FXMLLoader();
+
+        loader.setLocation(MainWindowController.class.getResource("editTour.fxml"));
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        EditTourController controller = loader.getController();
+        Stage stage = new Stage();
+        stage.setTitle("Edit Tour");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+    /**
+     * The method is used to open the addLog window,
+     * when you click at the edit button
+     **/
+    public void addLogWindow(ActionEvent actionEvent) {
+        Parent root=null;
+        FXMLLoader loader = new FXMLLoader();
+
+        loader.setLocation(MainWindowController.class.getResource("addLog.fxml"));
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        AddLogController controller = loader.getController();
+        Stage stage = new Stage();
+        stage.setTitle("Edit Tour");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    /**
+     * The method is used to open the editLog window,
+     * when you click at the edit button
+     **/
+    public void editLogWindow(ActionEvent actionEvent) {
+        Parent root=null;
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainWindowController.class.getResource("editLog.fxml"));
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        EditLogController controller = loader.getController();
+        Stage stage = new Stage();
+        stage.setTitle("Edit Tour");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
 }
