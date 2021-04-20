da280462206edfcf4751919031ed6b193f31f282
Arber Muca <if19b164@technikum-wien.at>
2021-04-20 19:27:11 +0200

    createTour method added

--- src/sample/viewModels/AddTourViewModel.java
+++ src/sample/viewModels/AddTourViewModel.java
@@ -4,6 +4,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import lombok.Getter;
 import lombok.Setter;
+import sample.models.Tour;
 
 public class AddTourViewModel {
 
@@ -16,5 +17,24 @@ public class AddTourViewModel {
     @Setter @Getter private final StringProperty inputDistance = new SimpleStringProperty("");
     @Setter @Getter private final StringProperty inputDescription = new SimpleStringProperty("");
 
-    //private final StringProperty output = new SimpleStringProperty("Norbert is the 0 who clicked");
+    public void addTour(){
+        String name = this.inputName.get();
+        String start =  this.inputStart.get();
+        String dest = this.inputDestination.get();
+        String dist = this.inputDistance.get();
+        String desc = this.inputDescription.get();
+
+        double distance = 0;
+        if (!dist.equals(""))
+            distance = Double.parseDouble(dist);
+
+        System.out.println("N:"+name+", S:"+start+", De:"+dest+", Di:"+distance+", Desc:"+desc);
+
+        Tour tour = new Tour(name,desc,start,dest,distance);
+        System.out.println("Objekt--> N:"+tour.getT_Name()+", S:"+tour.getStartPoint()+", De:"
+                +tour.getDestination()+", Di:"+tour.getT_Distance()+", Desc:"+tour.getDescription());
+
+        //database access
+        //database.addToDatabase(tour);
+    }
 }
