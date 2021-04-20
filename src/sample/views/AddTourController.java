79ca1a7d272c97b924234d30c30d9fece7e56368
Arber Muca <if19b164@technikum-wien.at>
2021-04-20 19:21:43 +0200

    addtour method added

--- src/sample/views/AddTourController.java
+++ src/sample/views/AddTourController.java
@@ -1,10 +1,13 @@
 package sample.views;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 import sample.viewModels.AddTourViewModel;
 
 public class AddTourController implements Initializable {
@@ -15,7 +18,19 @@ public class AddTourController implements Initializable {
     public TextField DestinationTextField;
     public TextField DistanceTextField;
     public TextArea DescriptionTextField;
-    public Button addBtn;
+    public Button AddBtn;
+
+    /**
+     * this method inserts the tour into the database or file
+     * and closes the window.
+     * */
+    @FXML
+    public void addTour(ActionEvent actionEvent) {
+        addTourViewModel.addTour();
+        //close the window
+        Stage stage = (Stage) AddBtn.getScene().getWindow();
+        stage.close();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
