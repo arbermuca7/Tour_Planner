15464860b7cef2fa6db3ee91553059ade5ae5f88
Arber Muca <if19b164@technikum-wien.at>
2021-06-01 10:33:05 +0200

    export file name geändert

--- src/sample/businessLayer/ExportFile/Export.java
+++ src/sample/businessLayer/ExportFile/Export.java
@@ -9,10 +9,15 @@ import sample.models.Tour;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class Export{
     private static final Logger logger = LogManager.getLogger(Export.class);
+
+    Date date = new Date() ;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss") ;
 
     public boolean ExportFile(List<Tour> tourList, String ordner) {
 
@@ -21,7 +26,7 @@ public class Export{
         TypeReference<List<Tour>> typeRef = new TypeReference<List<Tour>>() {};
         try {
             mapper.writerFor(typeRef)
-                    .writeValue(new File(ordner+"tours.json"),tourList);
+                    .writeValue(new File(ordner+dateFormat.format(date)+".json"),tourList);
         } catch (IOException e) {
             logger.error(e.getMessage());
             return false;
